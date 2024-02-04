adcc217e52a096c3cf96dfd9a2690c5721158c44
nulln0ne <nulln0ne@protonmail.com>
2024-02-04 16:31:29 +0300

    update

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "iosevka:size=11:antialias=true:autohint=true";
+static char *font = "iosevka nerd font:size=12:antialias=true:autohint=true";
 static char *font2[] = { "AppleColorEmoji:pixelsize=11:antialias=true:autohint=true" };
 static int borderpx = 1;
 
